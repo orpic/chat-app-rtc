683c0c3bbf2991e1bd44bb4aaeced53e0faae192
shobhit-u <shobhitumrao5@gmail.com>
2023-02-04 21:38:54 +0530

    chat header complete

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,8 +2,21 @@ import React from "react";
 // eslint-disable-next-line
 import styles from "./Chat.module.css";
 
+import { images } from "../../images";
+
 const Chat = () => {
-  return <div className={styles.container}>Chat</div>;
+  return (
+    <div className={styles.chat}>
+      <div className={styles.chatInfo}>
+        <span>Name</span>
+        <div className={styles.chatIcons}>
+          <img src={images.cam} alt="cam" />
+          <img src={images.add} alt="add" />
+          <img src={images.more} alt="more" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
